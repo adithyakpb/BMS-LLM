2c776fc4a146a50423c7b75d353447bd14d7f432
akptw <adithya.kp@thoughtworks.com>
2024-02-05 12:47:24 +0530

    Refactor. Event.py

--- BMS/Event.py
+++ BMS/Event.py
@@ -1,6 +1,6 @@
 import datetime
 from pymongo import MongoClient
-from databases.KnowledgeBase import KnowledgeBase
+from databases.knowledgebase import KnowledgeBase
 import hashlib
 class Event(KnowledgeBase):
     """
@@ -15,7 +15,7 @@ class Event(KnowledgeBase):
         description: description of the event
     """
     #constructor
-    def __init__(self, title: str, start_dt: str, end_dt: str, venue: str | None, org: str, fee: int | list, link: str | None, contact: str | None, description: str | None) -> None:
+    def __init__(self, title: str, start_dt: str, end_dt: str, venue: str | None, organizers: str, fee: int | list, link: str | None, contact: str | None, description: str | None) -> None:
         super().__init__("events-calendar")
         try:
             start_dt = datetime.datetime.strptime(start_dt, "%d-%m-%Y %H:%M")
@@ -23,24 +23,23 @@ class Event(KnowledgeBase):
         except ValueError:
             print("Please follow the format: DD-MM-YYYY HH:MM (24 hour format)")
             return
-        self.set_event_id
-        self.set_title(title)
-        self.set_start_datetime(start_dt)
-        self.set_end_datetime(end_dt)
-        self.set_venue(venue)
-        self.set_organizers(org)
-        self.set_fee(fee)
-        self.set_link(link)
-        self.set_contact(contact)
-        self.set_description(description)
+        self.title = title
+        self.start_datetime = start_dt
+        self.end_datetime = end_dt
+        self.venue = venue
+        self.organizers = organizers
+        self.fee = fee
+        self.link = link
+        self.contact = contact
+        self.description = description
 
-        self.event_template = f"The event named {self.title} is scheduled to be held from {self.start_datetime} to {self.end_datetime} at {self.venue}. The event is organized by {self.organizers} and the registration fee is {self.fee}. For more details, visit {self.link} or contact {self.contact}. The description of the event is as follows: {self.description}."
-
+        
     def __str__(self) -> str:
         return self.event_template
     
-    # def __repr__(self) -> str:
-    #     return "Event(title={}, start_datetime={}, end_datetime={}, venue={}, organizers={}, fee={}, link={}, contact={}, description={})".format(self._event_title, self._event_start_datetime, self._event_end_datetime, self._event_venue, self._event_organizers, self._event_fee, self._event_link, self._event_contact, self._event_description)
+    def __repr__(self) -> str:
+        return self.event_template
+    
     def to_vector(self) -> list:
         """Convert the event object to a vector to be inserted to a vectorstore."""
         from langchain_community.embeddings import HuggingFaceEmbeddings
@@ -57,53 +56,30 @@ class Event(KnowledgeBase):
         return Event(result["Title"], result["Start DateTime"], result["End DateTime"], result["Venue"], result["Organizers"], result["Registration Fee"], result["Link"], result["Contact"], result["Description"])
     def __dict__(self) -> dict:
         return {
-            "event_title": self._event_title,
-            "event_start_datetime": self._event_start_datetime,
-            "event_end_datetime": self._event_end_datetime,
-            "event_venue": self._event_venue,
-            "event_organizers": self._event_organizers,
-            "event_fee": self._event_fee,
-            "event_link": self._event_link,
-            "event_contact": self._event_contact,
-            "event_description": self._event_description
+            "Title": self.title,
+            "Start DateTime": self.start_datetime,
+            "End DateTime": self.end_datetime,
+            "Venue": self.venue,
+            "Organizers": self.organizers,
+            "Registration Fee": self.fee,
+            "Link": self.link,
+            "Contact": self.contact,
+            "Description": self.description
         }
+
     @property
-    def title(self):
-        return self._event_title
+    def event_template(self):
+        self.event_template = f"The event named {self.title} is scheduled to be held from {self.start_datetime} to {self.end_datetime} at {self.venue}. The event is organized by {self.organizers} and the registration fee is {self.fee}. For more details, visit {self.link} or contact {self.contact}. The description of the event is as follows: {self.description}."
+        return self.event_template
+
     @property
-    def start_datetime(self):
-        return self._event_start_datetime
-    @property
-    def end_datetime(self):
-        return self._event_end_datetime
-    @property
-    def venue(self):
-        return self._event_venue
-    @property
-    def organizers(self):
-        return self._event_organizers
-    @property
-    def fee(self):
-        return self._event_fee
-    @property
-    def link(self):
-        return self._event_link
-    @property
-    def contact(self):
-        return self._event_contact
-    @property
-    def description(self):
-        return self._event_description
-    
+    def event_id(self):
+        self.event_id = hashlib.md5(self.event_template.encode()).hexdigest()
+        return self.event_id
     def insert_to_db(self):
         client = MongoClient('mongodb://localhost:27017/')
         db = client['bms-kb']
         collection = db['events-calendar']
         collection.insert_one(self.__dict__)
-        
-    # def __eq__(self, o: object) -> bool:
-    #     return self.event_template == o.event_template
-    
-    # def __hash__(self) -> int:
-    #     return
+
 
