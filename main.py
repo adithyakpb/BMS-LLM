bee4093691857dbb98f854062d99a6b55479e4d3
akptw <adithya.kp@thoughtworks.com>
2024-02-05 16:03:41 +0530

    Add. main.py inference functionality

--- main.py
+++ main.py
@@ -2,19 +2,22 @@ import os
 from openai import OpenAI
 from dotenv import load_dotenv
 load_dotenv()
-client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+client = OpenAI(base_url="http://localhost:80/v1", api_key="not-needed")
 
 user_query = ""
 while user_query != "exit":
     user_query = ""
-    user_query = input("Enter your query:")
-    completion = client.chat.completions.create(
+    user_query = input("\nEnter your query:")
+    response = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
-        {"role": "system", "content": "You are BMS-Assistant, a chatbot that answers questions about BMS College of Engineering."},
+        {"role": "system", "content": "You are an AI assistant for an engineering college in Bengaluru, Karnataka, India. The name of the college is B.M.S College of Engineering. Your name is 'BMS-Bot'."},
         {"role": "assistant", "content" : "Please answer all questions ethically, honestly and responsibly. If you do not know the answer to a question, please say so."},
         {"role": "user", "content": f"{user_query}"},
-    ]
+    ],
+    stream=True,
+    temperature=0.2
     )
-    print(completion.choices[0].message.content)
+    for chunk in response:
+        print(chunk.choices[0].delta.content, end='', flush=True)
     
